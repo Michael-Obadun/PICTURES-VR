e96efb3290b67527fecb4f7b146a3a608d804408
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-28 09:16:03 +0000

    installed pillow and added it to requirements.txt

--- picturesVR/settings.py
+++ picturesVR/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
 ALLOWED_HOSTS = ['8000-michaelobadu-picturesvr-h99cqiq3tyk.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
