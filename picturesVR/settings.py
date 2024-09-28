815fa82e43d5062c86dac6bfd8b4afa60f9d9ef1
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-28 08:50:18 +0000

    updated procfile

--- picturesVR/settings.py
+++ picturesVR/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-)lbz*s#gz-^maeo0o$f)x-oskq^n_oy8&#(%nd7$9u)a_)82ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-michaelobadu-picturesvr-h99cqiq3tyk.ws.codeinstitute-ide.net'
-                ,'.herokuapp.com']
+ALLOWED_HOSTS = ['8000-michaelobadu-picturesvr-h99cqiq3tyk.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
